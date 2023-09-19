5e71c74e039109335e54eb308e5a29b81d72c3e4
dotnet-code <@dotnet-code>
2023-09-19 09:55:31 +0000

    commit from code202309195532

---
+++ Angular/src/app/customer-data/customer-data.component.ts
@@ -0,0 +1,27 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-customer-data',
+  templateUrl: './customer-data.component.html',
+  styleUrls: ['./customer-data.component.css']
+})
+export class CustomerDataComponent implements OnInit {
+  customerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.customerForm = this.formBuilder.group({
+      cardNumber: ['', [Validators.required, Validators.pattern('^[0-9]{16}$')]],
+      cvvCode: ['', [Validators.required, Validators.pattern('^[0-9]{3}$')]],
+      ePayment: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]]
+    });
+  }
+
+  saveCustomerData(): void {
+    if (this.customerForm.valid) {
+      // Implement API call to securely store customer data
+    }
+  }
+}
